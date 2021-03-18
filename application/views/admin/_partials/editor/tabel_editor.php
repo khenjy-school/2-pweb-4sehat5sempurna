bc92d02e9c3f564946b3d04d8d814cfc761501d4
Khenjy <khenjyjohnelson@outlook.com>
2021-03-18 15:17:28 +0700

    Edit 20210318 Part 3
    Perubahan desain tabel pada admin

--- application/views/admin/_partials/editor/tabel_editor.php
+++ application/views/admin/_partials/editor/tabel_editor.php
@@ -1,4 +1,4 @@
-<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+<table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
     <thead>
         <tr>
             <th>Id Editor</th>
